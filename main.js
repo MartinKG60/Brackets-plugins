35dc50afc4545d72a19c16fddb9c454e3b5d5edc
Martin Gammelholm <mga@1stweb.dk>
2014-08-25 13:52:50 +0200

    First push

---
+++ main.js
@@ -0,0 +1,31 @@
+define(function (require, exports, module) {
+    "use strict";
+    var commandName         = "addComment";
+    var Menus               = brackets.getModule("command/Menus"),
+        CommandManager      = brackets.getModule("command/CommandManager"),
+        DocumentManager     = brackets.getModule("document/DocumentManager"),
+        EditorManager       = brackets.getModule("editor/EditorManager");
+    /*
+     * Implementation
+     */
+    function addComment() {
+
+        var currentDoc = DocumentManager.getCurrentDocument();
+        var editor = EditorManager.getCurrentFullEditor();
+        var pos = editor.getCursorPos();
+        
+        var current_date = new Date().toDateString();
+        var comment = "<!-- MGA on " + current_date + ": " + prompt("Comment Text","") + " -->";
+        
+        currentDoc.replaceRange(comment, pos);
+    }
+    /*
+     * Command
+     */
+    CommandManager.register("Add Comment", commandName, addComment);
+    /*
+     * Custom menu
+     */
+    var menu = Menus.addMenu("Custom", "edgedocks.custom.menu");
+    menu.addMenuItem(commandName);
+});
